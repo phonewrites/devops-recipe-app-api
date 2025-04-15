5bb15ee32251663ec21ff94b441adb0b7a4891df
Maan Wardhan <171957343+phonewrites@users.noreply.github.com>
2025-04-15 08:03:04 +0300

    upd iam

--- infra/setup/vars.tf
+++ infra/setup/vars.tf
@@ -1,8 +1,8 @@
+# S3 bucket & DynamoDB table are created+managed outside Terraform
 variable "tf_state_bucket" {
   description = "Name of S3 bucket in AWS for storing TF state"
   default     = "tf-state-nvirginia-961341515801"
 }
-
 variable "tf_state_lock_table" {
   description = "Name of DynamoDB table for TF state locking"
   default     = "terraform-state-locks"
