5edf9b6078251efc100b404e55801e09a73622d4
Maan Wardhan <171957343+phonewrites@users.noreply.github.com>
2025-05-11 19:56:28 +0300

    add S3:ListObjectsV2 permission to cicd-gh-actions-role

--- infra/setup/iam_cicd_prod.tf
+++ infra/setup/iam_cicd_prod.tf
@@ -29,8 +29,13 @@ data "aws_iam_policy_document" "tf_backend_policy" {
     resources = ["arn:aws:s3:::${var.tf_state_bucket}"]
   }
   statement {
-    effect  = "Allow"
-    actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+      "s3:ListObjectsV2",
+    ]
     resources = [
       "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy/*",
       "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy-env/*"
