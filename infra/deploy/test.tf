b0a591b38fbac2eb6523cae18c0cc1ab9bcffbeb
Maan Wardhan <171957343+phonewrites@users.noreply.github.com>
2025-05-13 19:21:24 +0300

    upd

--- infra/deploy/test.tf
+++ infra/deploy/test.tf
@@ -1,6 +1,5 @@
 data "aws_caller_identity" "current" {}
-
 resource "aws_s3_bucket" "test_bucket" {
-  bucket        = "tygvbkjbjb-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.region}"
+  bucket        = "iocwheaifvnsak-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.region}"
   force_destroy = true
 }
