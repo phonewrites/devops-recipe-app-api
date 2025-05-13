4f8689a493535a517234c1c3c2114499666afa6c
Maan Wardhan <171957343+phonewrites@users.noreply.github.com>
2025-05-13 19:03:56 +0300

    test4 role_arn in backend

--- infra/deploy/test.tf
+++ infra/deploy/test.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "test_bucket" {
-  bucket        = "tygvbkjbjb-${data.aws_caller_identity.current.account_id}-${data.aws_region.region}"
+  bucket        = "tygvbkjbjb-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.region}"
   force_destroy = true
 }
