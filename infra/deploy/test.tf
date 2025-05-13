562f5d2a5a6285c4cc2a565d5b391732bc21cb27
Maan Wardhan <171957343+phonewrites@users.noreply.github.com>
2025-05-13 19:24:37 +0300

    test5 role_arn in backend

--- infra/deploy/test.tf
+++ infra/deploy/test.tf
@@ -2,4 +2,9 @@ data "aws_caller_identity" "current" {}
 resource "aws_s3_bucket" "test_bucket" {
   bucket        = "iocwheaifvnsak-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.region}"
   force_destroy = true
+  timeouts {
+    create = "5m"
+    delete = "6m"
+  }
+
 }
