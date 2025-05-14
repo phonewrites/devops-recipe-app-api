5acd53e6c655598fbf4cebf1e039f94076b0c102
Maan Wardhan <171957343+phonewrites@users.noreply.github.com>
2025-05-14 19:53:47 +0300

    test s3 create bug

--- infra/deploy/main-test.tf
+++ infra/deploy/main-test.tf
@@ -3,5 +3,5 @@
 
 resource "aws_s3_bucket" "test_s3_creating_loop" {
   bucket = "another-test-${data.aws_region.current.region}-${data.aws_caller_identity.current.account_id}"
-
+  force_destroy = true
 }
