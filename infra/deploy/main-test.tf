163d0f3ce4f724d3dc020dd374cfff007dbc70b6
Maan Wardhan <171957343+phonewrites@users.noreply.github.com>
2025-05-14 19:54:52 +0300

    test s3 force delete

--- infra/deploy/main-test.tf
+++ infra/deploy/main-test.tf
@@ -1,7 +1,7 @@
 
 # to test/fix s3 still creating loop 
 
-resource "aws_s3_bucket" "test_s3_creating_loop" {
-  bucket = "another-test-${data.aws_region.current.region}-${data.aws_caller_identity.current.account_id}"
-  force_destroy = true
-}
+# resource "aws_s3_bucket" "test_s3_creating_loop" {
+#   bucket = "another-test-${data.aws_region.current.region}-${data.aws_caller_identity.current.account_id}"
+#   force_destroy = true
+# }
