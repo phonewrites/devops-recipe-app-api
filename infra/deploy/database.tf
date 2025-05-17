fdc541e6e0a4a4cd892323e934af5f7583c8033e
Maan Wardhan <171957343+phonewrites@users.noreply.github.com>
2025-05-17 18:18:19 +0300

    del/fix

--- infra/deploy/database.tf
+++ infra/deploy/database.tf
@@ -6,40 +6,48 @@ resource "aws_db_subnet_group" "main" {
   }
 }
 
-resource "aws_security_group" "rds_inbound_access" {
-  name        = "${local.prefix}-rds-inbound-access"
-  description = "Access to the RDS DB instance"
-  vpc_id      = aws_vpc.main.id
-  ingress {
-    protocol  = "tcp"
-    from_port = 5432
-    to_port   = 5432
-  }
-  # lifecycle {
-  #   create_before_destroy = true #Fix "Still destroying..." issue
-  # }
-  tags = {
-    Name = "${local.prefix}-rds-inbound-access"
-  }
-}
+# resource "aws_security_group" "rds_inbound_access" {
+#   name        = "${local.prefix}-rds-inbound-access"
+#   description = "Access to the RDS DB instance"
+#   vpc_id      = aws_vpc.main.id
+#   ingress {
+#     protocol  = "tcp"
+#     from_port = 5432
+#     to_port   = 5432
+#   }
+#   lifecycle {
+#     create_before_destroy = true #Fix "Still destroying..." issue
+#   }
+#   tags = {
+#     Name = "${local.prefix}-rds-inbound-access"
+#   }
+# }
 
-resource "aws_db_instance" "main" {
-  identifier                 = "${local.prefix}-db"
-  db_name                    = local.prefix
-  allocated_storage          = 20
-  storage_type               = "gp2"
-  engine                     = "postgres"
-  engine_version             = "15.3"
-  auto_minor_version_upgrade = true
-  instance_class             = "db.t4g.micro"
-  username                   = var.db_username
-  password                   = var.db_password
-  skip_final_snapshot        = true
-  db_subnet_group_name       = aws_db_subnet_group.main.name
-  multi_az                   = false
-  backup_retention_period    = 0
-  vpc_security_group_ids     = [aws_security_group.rds_inbound_access.id]
-  tags = {
-    Name = "${local.prefix}-db"
-  }
-}
+# resource "aws_vpc_security_group_ingress_rule" "rds_inbound_access" {
+#   security_group_id = aws_security_group.rds_inbound_access.id
+#   cidr_ipv4   = []
+#     from_port = 5432
+#   ip_protocol = "tcp"
+#     to_port   = 5432
+# }
+
+# resource "aws_db_instance" "main" {
+#   identifier                 = "${local.prefix}-db"
+#   db_name                    = local.prefix
+#   allocated_storage          = 20
+#   storage_type               = "gp2"
+#   engine                     = "postgres"
+#   engine_version             = "15.3"
+#   auto_minor_version_upgrade = true
+#   instance_class             = "db.t4g.micro"
+#   username                   = var.db_username
+#   password                   = var.db_password
+#   skip_final_snapshot        = true
+#   db_subnet_group_name       = aws_db_subnet_group.main.name
+#   multi_az                   = false
+#   backup_retention_period    = 0
+#   vpc_security_group_ids     = [aws_security_group.rds_inbound_access.id]
+#   tags = {
+#     Name = "${local.prefix}-db"
+#   }
+# }
