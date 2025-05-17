74f3437dc3008f6d142cf971450854336bf074da
Maan Wardhan <171957343+phonewrites@users.noreply.github.com>
2025-05-17 20:28:40 +0300

    upd to separate rule

--- infra/deploy/database.tf
+++ infra/deploy/database.tf
@@ -5,30 +5,31 @@ resource "aws_db_subnet_group" "main" {
     Name = "${local.prefix}-db-subnet-group"
   }
 }
-# resource "aws_security_group" "rds_inbound_access" {
-#   name        = "${local.prefix}-rds-inbound-access"
-#   description = "Access to the RDS DB instance"
-#   vpc_id      = aws_vpc.main.id
-#   # ingress {
-#   #   cidr_blocks = [for cidr in local.private_cidrs : cidr]
-#   #   protocol    = "tcp"
-#   #   from_port   = 5432
-#   #   to_port     = 5432
-#   # }
-#   lifecycle {
-#     create_before_destroy = true #Fix "Still destroying..." issue
-#   }
-#   tags = {
-#     Name = "${local.prefix}-rds-inbound-access"
-#   }
-# }
-# resource "aws_vpc_security_group_ingress_rule" "rds_inbound_rules" {
-#   security_group_id = aws_security_group.rds_inbound_access.id
-#   cidr_ipv4         = "10.127.128.0/24"
-#   from_port         = 5432
-#   to_port           = 5432
-#   ip_protocol       = "tcp"
-# }
+resource "aws_security_group" "rds_inbound_access" {
+  name        = "${local.prefix}-rds-inbound-access"
+  description = "Access to the RDS DB instance"
+  vpc_id      = aws_vpc.main.id
+  # ingress {
+  #   cidr_blocks = [for cidr in local.private_cidrs : cidr]
+  #   protocol    = "tcp"
+  #   from_port   = 5432
+  #   to_port     = 5432
+  # }
+  lifecycle {
+    create_before_destroy = true #Fix "Still destroying..." issue
+  }
+  tags = {
+    Name = "${local.prefix}-rds-inbound-access"
+  }
+}
+resource "aws_vpc_security_group_ingress_rule" "rds_inbound_rules" {
+  for_each          = toset(local.private_cidrs)
+  security_group_id = aws_security_group.rds_inbound_access.id
+  cidr_ipv4         = each.value
+  from_port         = 5432
+  to_port           = 5432
+  ip_protocol       = "tcp"
+}
 
 
 # resource "aws_db_instance" "main" {
