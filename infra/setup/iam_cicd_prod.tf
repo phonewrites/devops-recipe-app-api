fa4f2aa70163840728ca1c688d395fc8a707617f
Maan Wardhan <171957343+phonewrites@users.noreply.github.com>
2025-05-20 11:42:30 +0300

    sans suffix

--- infra/setup/iam_cicd_prod.tf
+++ infra/setup/iam_cicd_prod.tf
@@ -131,8 +131,8 @@ data "aws_iam_policy_document" "cicd_gh_actions_policy" {
       "iam:DeleteServiceLinkedRole"
     ]
     resources = [
-      "arn:aws:iam::*:role/aws-service-role/rds.amazonaws.com/AWSServiceRoleForRDS",
-      "arn:aws:iam::*:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS*",#try suffix
+      "arn:aws:iam::*:role/aws-service-role/rds.amazonaws.com/AWSServiceRoleForRDS*", #try suffix
+      "arn:aws:iam::*:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS",
     ]
     condition {
       test     = "StringEquals"
