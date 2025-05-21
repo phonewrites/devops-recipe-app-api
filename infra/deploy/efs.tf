e6d27df29542a546235558e7e06565121cc61e01
Maan Wardhan <171957343+phonewrites@users.noreply.github.com>
2025-05-21 12:43:35 +0300

    EFS access point

--- infra/deploy/efs.tf
+++ infra/deploy/efs.tf
@@ -1,17 +1,28 @@
 ########## TESTING ####################
 
 # EFS resources for persistent media storage
-resource "aws_efs_file_system" "main" {
+resource "aws_efs_file_system" "media" {
   encrypted = true #Encryption at rest
   tags = {
     Name = "${local.prefix}-media"
   }
 }
-resource "aws_efs_mount_target" "main" {
+resource "aws_efs_mount_target" "media" {
   for_each        = aws_subnet.private
-  file_system_id  = aws_efs_file_system.main.id
+  file_system_id  = aws_efs_file_system.media.id
   subnet_id       = each.value.id
   security_groups = [aws_security_group.efs_access.id]
+}
+resource "aws_efs_access_point" "media" {
+  file_system_id = aws_efs_file_system.media.id
+  root_directory {
+    path = "/api/media"
+    creation_info {
+      owner_gid   = 101
+      owner_uid   = 101
+      permissions = "755"
+    }
+  }
 }
 
 # Security Group to implement Access Control to EFS
