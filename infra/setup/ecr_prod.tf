0b686bc964dc1ef7d3078d59e2e0236c44e91dc9
Maan Wardhan <171957343+phonewrites@users.noreply.github.com>
2025-05-22 21:05:54 +0300

    ECR image lifecycle policy

--- infra/setup/ecr_prod.tf
+++ infra/setup/ecr_prod.tf
@@ -8,62 +8,11 @@ resource "aws_ecr_repository" "recipe_app_api_app" {
     scan_on_push = false #Set true for real deployments.
   }
 }
-# resource "aws_ecr_lifecycle_policy" "recipe_app_api_app" {
-#   repository = aws_ecr_repository.recipe_app_api_app.name
-#   policy = data.aws_ecr_lifecycle_policy_document.recipe_app_api_app.json
-# }
-# data "aws_ecr_lifecycle_policy_document" "recipe_app_api_app" {
-#   rule {
-#     priority    = 1
-#     description = "Keep last 10 images"
-#     selection {
-#       tag_status      = "tagged"
-#       tag_prefix_list = ["prod"]
-#       count_type      = "imageCountMoreThan"
-#       count_number    = 100
-#     }
-#   }
-# }
-
-
-#   lifecycle_policy = jsonencode({
-#     rules = [
-#       {
-#         rulePriority = 1
-#         selection = {
-#           tagStatus = "any"
-#           countType = "imageCountMoreThan"
-#           countNumber = 10
-#         }
-#         action = {
-#           type = "expire"
-#         }
-#       }
-#     ]
-#   })
-
-#   policy = <<EOF
-# {
-#     "rules": [
-#         {
-#             "rulePriority": 1,
-#             "description": "Keep last 10 images",
-#             "selection": {
-#                 "tagStatus": "any",
-#                 "tagPrefixList": ["v"],
-#                 "countType": "imageCountMoreThan",
-#                 "countNumber": 30
-#             },
-#             "action": {
-#                 "type": "expire"
-#             }
-#         }
-#     ]
-# }
-# EOF
-
-
-
+resource "aws_ecr_lifecycle_policy" "recipe_app_api_app" {
+  provider   = aws.prod
+  repository = aws_ecr_repository.recipe_app_api_app.name
+  policy     = data.aws_ecr_lifecycle_policy_document.retain_last_10_tagged.json
+}
 resource "aws_ecr_repository" "recipe_app_api_proxy" {
   name                 = "recipe-app-api-proxy"
   provider             = aws.prod
@@ -71,5 +20,24 @@ resource "aws_ecr_repository" "recipe_app_api_proxy" {
   force_delete         = true #Set for demo purposes only
   image_scanning_configuration {
     scan_on_push = false #Set true for real deployments.
+  }
+}
+resource "aws_ecr_lifecycle_policy" "recipe_app_api_proxy" {
+  provider   = aws.prod
+  repository = aws_ecr_repository.recipe_app_api_proxy.name
+  policy     = data.aws_ecr_lifecycle_policy_document.retain_last_10_tagged.json
+}
+data "aws_ecr_lifecycle_policy_document" "retain_last_10_tagged" {
+  rule {
+    priority = 1
+    selection {
+      tag_status       = "tagged"
+      tag_pattern_list = ["*"]
+      count_type       = "imageCountMoreThan"
+      count_number     = 10
+    }
+    action {
+      type = "expire"
+    }
   }
 }
