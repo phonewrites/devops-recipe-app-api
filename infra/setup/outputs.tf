0eff1216122d30eff021530ef263dbf2e81e2717
Maan Wardhan <171957343+phonewrites@users.noreply.github.com>
2025-05-25 10:35:06 +0300

    push to prod

--- infra/setup/outputs.tf
+++ infra/setup/outputs.tf
@@ -1,9 +1,9 @@
-# Outputs for ECR repositories for app and proxy images #
-output "ecr_repo_app_uri" {
-  description = "ECR repository URL for app image"
-  value       = aws_ecr_repository.recipe_app_api_app.repository_url
-}
-output "ecr_repo_proxy_uri" {
-  description = "ECR repository URL for the proxy image"
-  value       = aws_ecr_repository.recipe_app_api_proxy.repository_url
-}
+# # Outputs for ECR repositories for app and proxy images #
+# output "ecr_repo_app_uri" {
+#   description = "ECR repository URL for app image"
+#   value       = aws_ecr_repository.recipe_app_api_app.repository_url
+# }
+# output "ecr_repo_proxy_uri" {
+#   description = "ECR repository URL for the proxy image"
+#   value       = aws_ecr_repository.recipe_app_api_proxy.repository_url
+# }
