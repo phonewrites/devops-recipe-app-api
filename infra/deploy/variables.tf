e0162f81fe31998b023ad375f08a7e644364f25a
Maan Wardhan <171957343+phonewrites@users.noreply.github.com>
2025-05-25 12:39:43 +0300

    fix fmt

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -37,7 +37,7 @@ variable "django_secret_key" {
 
 variable "custom_domain" {
   description = "Your Route53 hosted zone name (e.g. example.com)"
-  type = string
+  type        = string
 }
 variable "subdomain" {
   description = "Subdomain for each environment"
