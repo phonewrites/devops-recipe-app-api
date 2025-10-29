553b44ae64bc4503cdb2376615435955a060ed0d
Maan Wardhan <171957343+phonewrites@users.noreply.github.com>
2025-10-29 07:11:53 +0200

    build(deploy) yolo: upgrade AWS provider to latest beta

--- infra/setup/providers.tf
+++ infra/setup/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.0.0-beta1"
+      version = ">= 6.0.0-0, < 6.0.0"
     }
   }
   # Terraform state backend configuration in mgmt account
